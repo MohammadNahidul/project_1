974fdd44996f3c50eb18f1d6728ba47a7bd3ce1e
henryjack166 <75333021+henryjack166@users.noreply.github.com>
2022-02-14 11:46:12 +0600

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Nahidu islam</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<section class="header">
+    <div class="container">
+        <div class="row">
+            <dis class="col-6">
+                <h4>This is first time work</h4>
+            </dis>
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
